f3f75f7cea55371de4d5f9130e697269494fee2a
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-09-20 09:31:19 +0200

    commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -25,7 +25,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
         py-3 
         disabled:cursor-not-allowed 
         disabled:opacity-50
-        text-black
+        text-blue-950
         font-bold
         hover:opacity-75
         transition
